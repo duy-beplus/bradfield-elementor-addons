30f477f366a7f645ffb2e90863355900cc407739
Tuan <tuan.beplus@gmail.com>
2022-07-18 15:23:31 +0700

    update Schedule View

--- assets/js/single-event-script.js
+++ assets/js/single-event-script.js
@@ -5,11 +5,13 @@ $(document).ready(function(){
 
     var month = $('.bradfield-event-date .evo_start .month').text();
     var date = $('.bradfield-event-date .evo_start .date').text();
+    // var date_slice = date.slice(1);
     var time = $('.bradfield-event-date .evo_eventcard_time_t').text();
     // get date time event in a month
-    date_time = date + " " + time.split(date) ;
+    // date_time = date_slice + " " + time.split(date_slice) ;
     // get date time event lasted for many months
-    month_date_time = month + " " + date_time.split(month);
+    // month_date_time = month + " " + date_time.split(month);
+    month_date_time = month + " " + date + " " + time;
     format_month_date_time = month_date_time.replace(/, /g, '');
     $('.bradfield-event-date .evo_eventcard_time_t').html(format_month_date_time);
   });
